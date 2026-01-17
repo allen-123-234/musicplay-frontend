9c18b0d664c63e326a75adb7d143990742ab1a9c
allen-123-234 <115543347+allen-123-234@users.noreply.github.com>
2026-01-18 01:12:47 +0800

    script.js

--- script-v7.js
+++ script-v7.js
@@ -1,8 +1,11 @@
 const songList = document.getElementById("song-list")
 
-fetch("http://localhost:3000/api/songs")
+fetch("https://musicplay-backend.onrender.com/api/songs")
   .then(res => res.json())
   .then(songs => {
+    // 清空列表，避免重複 append
+    songList.innerHTML = ""
+
     songs.forEach(song => {
       const li = document.createElement("li")
       li.textContent = `${song.title} - ${song.artist}`
